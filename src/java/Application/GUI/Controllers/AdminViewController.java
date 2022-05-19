ad6a592bc1f4e3c2242573bc4e7a371135630731
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-19 16:32:23 +0200

    Updated CreateStudentView to be better structures and consistent with other menus

--- src/java/Application/GUI/Controllers/AdminViewController.java
+++ src/java/Application/GUI/Controllers/AdminViewController.java
@@ -6,43 +6,41 @@ import Application.GUI.Models.SchoolModel;
 import Application.GUI.Models.StudentModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AdminViewController implements Initializable {
 
-    @FXML
-    private TableView<StudentModel> tblViewStudent;
-    @FXML
-    private TableColumn<StudentModel, String> tblClmStudentFirstName;
-    @FXML
-    private TableColumn<StudentModel, String> tblClmStudentLastName;
-    @FXML
-    private TableColumn<StudentModel, String> tblClmStudentEmail;
-    @FXML
-    private TableColumn<StudentModel, String> tblClmStudentClass;
+    @FXML Button btnCreateTeacher;
+    @FXML Button btnCreateStudent;
+    @FXML Button btnCreateSchool;
+    
+    @FXML private TableView<StudentModel> tblViewStudent;
+    @FXML private TableColumn<StudentModel, String> tblClmStudentFirstName;
+    @FXML private TableColumn<StudentModel, String> tblClmStudentLastName;
+    @FXML private TableColumn<StudentModel, String> tblClmStudentEmail;
+    @FXML private TableColumn<StudentModel, String> tblClmStudentClass;
 
-    @FXML
-    private TableView<Account> tblViewTeacher;
-    @FXML
-    private TableColumn<Account, String> tblClmTeacherFirstName;
-    @FXML
-    private TableColumn<Account, String> tblClmTeacherLastName;
-    @FXML
-    private TableColumn<Account, String> tblClmTeacherEmail;
+    @FXML private TableView<Account> tblViewTeacher;
+    @FXML private TableColumn<Account, String> tblClmTeacherFirstName;
+    @FXML private TableColumn<Account, String> tblClmTeacherLastName;
+    @FXML private TableColumn<Account, String> tblClmTeacherEmail;
 
-    @FXML
-    private TableView<SchoolModel> tblViewSchool;
-    @FXML
-    private TableColumn<SchoolModel, String> tblClmSchoolName;
-    @FXML
-    private TableColumn<SchoolModel, Number> tblClmSchoolZipCode;
-    @FXML
-    private TableColumn<SchoolModel, String> tblClmSchoolCity;
+    @FXML private TableView<SchoolModel> tblViewSchool;
+    @FXML private TableColumn<SchoolModel, String> tblClmSchoolName;
+    @FXML private TableColumn<SchoolModel, Number> tblClmSchoolZipCode;
+    @FXML private TableColumn<SchoolModel, String> tblClmSchoolCity;
 
 
 
@@ -61,6 +59,47 @@ public class AdminViewController implements Initializable {
         tblClmSchoolCity.setCellValueFactory(param -> param.getValue().getCity());
     }
 
-    public void openCreateSchool(ActionEvent actionEvent) {
+    public void createStudent(ActionEvent actionEvent) throws IOException
+    {
+        try {
+            Stage popupMenu = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/Popups/CreateStudentView.fxml")));
+            popupMenu.setTitle("Ny Elev");
+            popupMenu.setScene(new Scene(root));
+            popupMenu.show();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void createTeacher(ActionEvent actionEvent)
+    {
+        try
+        {
+            Stage popupMenu = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/Popups/CreateTeacherView.fxml")));
+            popupMenu.setTitle("Ny Elev");
+            popupMenu.setScene(new Scene(root));
+            popupMenu.show();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void createSchool(ActionEvent actionEvent)
+    {
+        try
+        {
+            Stage popupMenu = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/Popups/CreateSchoolView.fxml")));
+            popupMenu.setTitle("Ny Skole");
+            popupMenu.setScene(new Scene(root));
+            popupMenu.show();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
