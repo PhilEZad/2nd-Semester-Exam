ce1788563ad67c4f6d1fdc7d6f744aeb79b15dcd
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-22 01:28:49 +0200

    Teacher student account CRUDs

--- src/java/Application/BLL/TeacherDataManager.java
+++ src/java/Application/BLL/TeacherDataManager.java
@@ -2,14 +2,17 @@ package Application.BLL;
 
 import Application.BE.*;
 import Application.DAL.*;
+import Application.GUI.Models.AccountModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TeacherDataManager
 {
+    TemplatePatternDAO accountDAO;
     TemplatePatternDAO inquiryDAO;
-    TemplatePatternDAO caseDAO;
     TemplatePatternDAO healthCategoryDAO;
     TemplatePatternDAO functionalAbilityDAO;
     TemplatePatternDAO citizenDAO;
@@ -19,17 +22,105 @@ public class TeacherDataManager
 
     public TeacherDataManager()
     {
+        accountDAO = new AccountDAO();
         citizenDAO = new CitizenDAO();
         categoryDAO = new CategoryDAO();
     }
 
 
-
-
-
-
     // Create/Read/Update/Delete - student (AccountDAO)
-        // single / all
+    // single / all
+
+    public AccountModel createStudent(Account account)
+    {
+       AccountModel accountModel = new AccountModel(
+            (Account) accountDAO.create(account)
+        );
+
+       return accountModel;
+    }
+
+    /**
+     * Fetches an Account model from Database and returns it as an AccountModel object.
+     * If account is null, makes a new AccountModel object with ID -1.
+     * @param account
+     * @return
+     */
+
+    public AccountModel getStudent(Account account)
+    {
+        AccountModel accountModel;
+
+        if(account != null)
+        {
+            accountModel = new AccountModel(
+                    (Account) accountDAO.read(account.getId())
+                    );
+
+            return accountModel;
+        } else {
+            accountModel = new AccountModel(
+                    new Account(
+                            -1,
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            new School(),
+                            2
+                    ));
+            return accountModel;
+        }
+    }
+
+    /**
+     * Fetches a list of all accounts, sorts them by privilege and returns an ObservableList of only student AccountModel objects.
+     * @return ObservableList<AccountModel>
+     */
+    public ObservableList<AccountModel> getAllStudents()
+    {
+        ObservableList<AccountModel> returnList = FXCollections.observableArrayList();
+        List<Account> accountList = accountDAO.readAll();
+
+        for (Account account : accountList)
+        {
+            if (account.getAuthorization() == 2)
+            {
+                AccountModel accountModel = new AccountModel(account);
+                returnList.add(accountModel);
+            }
+        }
+        return returnList;
+    }
+
+    public Boolean updateStudent(Account account)
+    {
+        if (account != null)
+        {
+            accountDAO.update(account);
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Deletes an account based on account ID, returns true if not null, false if null.
+     * @param account
+     * @return Boolean
+     */
+    public Boolean deleteStudent(Account account)
+    {
+        if (account != null)
+        {
+            accountDAO.delete(account.getId());
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     // Create/Read/Update/Delete - citizen (template) (CitizenDAO)
         // single / all
--- src/java/Application/GUI/Controllers/dashboard/StudentsController.java
+++ src/java/Application/GUI/Controllers/dashboard/StudentsController.java
@@ -1,11 +1,15 @@
 package Application.GUI.Controllers.dashboard;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class StudentsController {
     public AnchorPane anchorPaneStudents;
