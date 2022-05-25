86436e0c75133021b983eba9595a7f7f7ce5ab16
Mads Mandahl-Barth <mads-barth@live.dk>
2022-05-25 22:03:08 +0200

    Update CategoryDAO.java

--- src/java/Application/DAL/CategoryDAO.java
+++ src/java/Application/DAL/CategoryDAO.java
@@ -1,98 +1,148 @@
 package Application.DAL;
 
+import Application.BE.Account;
 import Application.BE.Category;
+import Application.BE.School;
 import Application.DAL.DBConnector.DBConnectionPool;
+import Application.DAL.TemplateMethod.AbstractDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CategoryDAO implements IDatabaseActions<Category> {
+public class CategoryDAO implements IDatabaseActions<Category>
+{
     @Override
-    public Category create(Category input) {
-        System.err.println("illegal");
-        return null;
-    }
-
-    @Override
-    public void update(Category input) {
-        System.err.println("illegal");
-    }
-
-    @Override
-    public Category read(int id) {
-
-        String sql = """
-                        SELECT CHILD.CatID, CHILD.catName, PARENT.CatID AS ParentID, PARENT.catName AS parentName 
-                        FROM dbo.Categories PARENT, dbo.Categories CHILD 
-                        WHERE PARENT.CatID = CHILD.FK_ParentCat AND CHILD.CatID = ?
-                        UNION SELECT NULLABLE.CatID, NULLABLE.catName, NULL, NULL
-                        FROM dbo.Categories AS NULLABLE 
-                        WHERE NULLABLE.FK_ParentCat IS NULL AND NULLABLE.CatID = ?
-                    """;
-
-        Connection conn = DBConnectionPool.getInstance().checkOut();
-
-        try {
-            PreparedStatement psas = conn.prepareStatement(sql);
-
-            psas.setInt(1, id);
-            psas.setInt(2, id);
-
-            ResultSet rs = psas.executeQuery();
-            rs.next();
-
-            return new Category(rs.getInt("CatID"), rs.getString("catName"), rs.getInt("ParentID"));
-
-        } catch (Exception e)
+    public Category create(Category input)
+    {
+        var dao = new AbstractDAO<Category>()
         {
-            e.printStackTrace();
-            return null;
-        }
-        finally {
-            DBConnectionPool.getInstance().checkIn(conn);
-        }
-    }
-
-    @Override
-    public List<Category> readAll() {
-        List<Category> categories = new ArrayList<>();
-
-        String sql = """
-                        SELECT CHILD.CatID, CHILD.catName, PARENT.CatID AS ParentID, PARENT.catName AS parentName 
-                        FROM dbo.Categories PARENT, dbo.Categories CHILD 
-                        WHERE PARENT.CatID = CHILD.FK_ParentCat 
-                        UNION SELECT NULLABLE.CatID, NULLABLE.catName, NULL, NULL
-                        FROM dbo.Categories AS NULLABLE WHERE NULLABLE.FK_ParentCat IS NULL
-                     """;
-
-        Connection conn = DBConnectionPool.getInstance().checkOut();
-
-        try {
-            PreparedStatement psas = conn.prepareStatement(sql);
-
-            ResultSet rs = psas.executeQuery();
-
-            while (rs.next())
+            @Override
+            protected Category execute(PreparedStatement statement) throws SQLException
             {
-                categories.add(new Category(rs.getInt("CatID"), rs.getString("catName"), rs.getInt("ParentID")));
+                AbstractDAO.setPlaceholders(statement, input.getName(), input.getParentID(), null, null);
+                statement.execute();
+                return input;
             }
 
-            return categories;
-        } catch (Exception e)
+            @Override
+            protected String getSQLStatement() {
+                return """
+                        INSERT INTO Categories (categoryName, FK_ParentCat, FK_Description, FK_Guide) 
+                        VALUES (?, ?, ?, ?)
+                        """;
+            }
+        };
+
+        dao.start();
+
+        if (dao.isSuccessful())
+            input.setID(dao.getResult().getKey());
+
+        return input;
+    }
+
+    @Override
+    public void update(Category input)
+    {
+        var dao = new AbstractDAO<Category>()
         {
-            e.printStackTrace();
-            return null;
-        }
-        finally {
-            DBConnectionPool.getInstance().checkIn(conn);
-        }
+            @Override
+            protected Category execute(PreparedStatement statement) throws SQLException
+            {
+                // TODO: 25-05-2022 missing foreign keys
+                AbstractDAO.setPlaceholders(statement, input.getName(), input.getParentID(), null, null, input.getID());
+                statement.execute();
+                return input;
+            }
+
+            @Override
+            protected String getSQLStatement() {
+                return """
+                        UPDATE Categories SET categoryName = ?, FK_ParentCat = ?, FK_Description = ?, FK_Guide = ?
+                        WHERE CatID = ?
+                        """;
+            }
+        };
+
+        dao.start();
+    }
+
+    @Override
+    public Category read(int id)
+    {
+        var dao = new AbstractDAO<Category>()
+        {
+            @Override
+            protected Category execute(PreparedStatement statement) throws SQLException
+            {
+                AbstractDAO.setPlaceholders(statement, id, id);
+                ResultSet rs = statement.executeQuery();
+
+                rs.next();
+
+                return new Category(rs.getInt("CatID"), rs.getString("catName"), rs.getInt("ParentID"));
+            }
+
+            @Override
+            protected String getSQLStatement() {
+                return """
+                        SELECT CHILD.CatID, CHILD.categoryName, PARENT.CatID AS ParentID, PARENT.categoryName AS parentName 
+                        FROM dbo.Categories PARENT, dbo.Categories CHILD 
+                        WHERE PARENT.CatID = CHILD.FK_ParentCat AND CHILD.CatID = ?
+                        UNION SELECT NULLABLE.CatID, NULLABLE.categoryName, NULL, NULL
+                        FROM dbo.Categories AS NULLABLE 
+                        WHERE NULLABLE.CatID = ?
+                        """;
+            }
+        };
+
+        dao.start();
+
+        return dao.getResult().getValue();
+    }
+
+    @Override
+    public List<Category> readAll()
+    {
+        var dao = new AbstractDAO<List<Category>>()
+        {
+            @Override
+            protected List<Category> execute(PreparedStatement statement) throws SQLException
+            {
+                ResultSet rs = statement.executeQuery();
+
+                var categories = new ArrayList<Category>();
+
+                while (rs.next())
+                {
+                    categories.add(new Category(rs.getInt("CatID"), rs.getString("catName"), rs.getInt("ParentID")));
+                }
+
+                return categories;
+            }
+
+            @Override
+            protected String getSQLStatement() {
+                return """
+                        SELECT CHILD.CatID, CHILD.categoryName, PARENT.CatID AS ParentID, PARENT.categoryName AS parentName 
+                        FROM dbo.Categories PARENT, dbo.Categories CHILD 
+                        WHERE PARENT.CatID = CHILD.FK_ParentCat
+                        """;
+            }
+        };
+
+        dao.start();
+
+        return dao.getResult().getValue();
     }
 
     @Override
     public void delete(int id) {
+        // TODO: 25-05-2022 ???
         System.err.println("illegal");
     }
 
