2866e9a7d7524d312b66dffd3dcbaa01142e9d11
Mads Mandahl-Barth <mads-barth@live.dk>
2022-05-25 18:24:12 +0200

    removed experimental classes

--- src/java/Application/DAL/TemplateMethod/Annotations/SQLColumn.java
+++
@@ -1,9 +0,0 @@
-package Application.DAL.TemplateMethod.Annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface SQLColumn {
-    String name() default "";
-}
