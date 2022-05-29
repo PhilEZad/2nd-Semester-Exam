799215e5673542868685b3ab5bdeebab6c630f4c
Mads Mandahl-Barth <mads-barth@live.dk>
2022-05-29 17:55:09 +0200

    removed duplicate

--- src/java/Application/GUI/Controllers/LoginController.java
+++ src/java/Application/GUI/Controllers/LoginController.java
@@ -1,21 +1,19 @@
 package Application.GUI.Controllers;
 
 import Application.BLL.SessionManager;
-import Application.GUI.Models.SessionModel;
 import Application.Utility.AccountType;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ListResourceBundle;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
